6a84908abd57175731ce8d5946521f90a2e5e1c1
maureen <maureen.chelangat@student.moringaschool.com>
2023-12-02 06:49:53 +0300

    completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+       
+        Song.count += 1
+
+        Song.artists.append(artist)
+        Song.artist_count = {artist: Song.artists.count(artist) for artist in set(Song.artists)}
+
+        Song.genres.append(genre)
+        Song.genre_count = {genre: Song.genres.count(genre) for genre in set(Song.genres)}
+
+
+
+
+        
+        
+        
+   
